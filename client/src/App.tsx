d6457536c39a5a01c0d59f343e83b3dd2ef6cece
Sayantan <sayantann2002@gmail.com>
2023-11-05 03:40:36 +0530

    Feat: Footer and Custom scrollbar added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,15 @@
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="mockup-window border m-10 h-auto bg-base-300">
       <div className="flex flex-col gap-5 justify-center bg-base-200">
         <Navbar />
-        <Hero/>
+        <Hero />
       </div>
+      <Footer />
     </div>
   );
 }
